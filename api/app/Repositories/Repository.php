28e5c834cb84b091f2684cdbb67b9d2e7aabba9d
Cassio <csmlvxi@gmail.com>
2025-01-25 17:03:30 -0300

    chore: remove methods from repository

--- api/app/Repositories/Repository.php
+++ api/app/Repositories/Repository.php
@@ -9,21 +9,6 @@ Class Repository implements RepositoryInterface
         return $this->model->find($id);
     }
 
-    public function getAll()
-    {
-        return $this->model->all();
-    }
-
-    public function getByAttr(string $attr, string $value)
-    {
-        return $this->model->where($attr, $value)->get();
-    }
-
-    public function getFirstByAttr(string $attr, string $value)
-    {
-        return $this->model->where($attr, $value)->first();
-    }
-
     public function save(array $data)
     {
         return $this->model->create($data);
@@ -36,10 +21,5 @@ Class Repository implements RepositoryInterface
     public function delete(string $id)
     {
         return $this->model->where('id', $id)->delete();
-    }
-
-    public function getByAttributeIn(string $field, array $attributes)
-    {
-        return $this->model->whereIn($field, $attributes)->get();
     }
 }
--- api/app/Repositories/RepositoryInterface.php
+++ api/app/Repositories/RepositoryInterface.php
@@ -5,11 +5,7 @@ namespace App\Repositories;
 interface RepositoryInterface
 {
     public function getById(string $id);
-    public function getAll();
-    public function getByAttr(string $attr, string $value);
-    public function getFirstByAttr(string $attr, string $value);
     public function save(array $data);
     public function update(string $id, array $data);
     public function delete(string $id);
-    public function getByAttributeIn(string $field, array $attributes);
 }
