a0b2a560c299bc4cf0f6682356f744d5de51cea0
Cassio <csmlvxi@gmail.com>
2025-01-30 18:15:01 -0300

    feat: add Email Notify dispatcher

---
+++ api/app/Notifications/EmailNotify.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailNotify extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $dados;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($dados)
+    {
+        $this->dados = $dados;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('Price of Item are updated')
+                    ->greeting('Hello,  the item ID' . $this->dados['id'] . 'has been updated!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
